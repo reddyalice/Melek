e3e028df45fbee81b47e69e5b62aa2e63b4b8c3d
Bahar Demircan <kodyenler@hotmail.com>
2021-06-10 21:31:24 +0300

    Running tests

---
+++ target/classes/shaders/Basic3DShader.glsl
@@ -0,0 +1,36 @@
+#shader vertex
+#version 400 core
+
+in vec3 position;
+in vec2 texCoords;
+in vec3 normal;
+
+out vec2 pass_texCoords;
+
+uniform mat4 transformationMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+
+void main(void){
+
+    vec4 worldPosition = transformationMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * viewMatrix * worldPosition;
+
+    pass_texCoords = texCoords;
+
+}
+
+
+    #shader fragment
+    #version 400 core
+
+in vec2 pass_texCoords;
+out vec4 out_Color;
+
+uniform sampler2D textureSampler;
+
+void main(void){
+
+    out_Color = texture(textureSampler, pass_texCoords);
+}
