7d6f50aa33ceeb8c34df2c1ead86c0ecf75e8d6a
Bahar Demircan <kodyenler@hotmail.com>
2021-07-20 19:17:28 +0300

    Trying things out

--- src/main/java/com/alice/mel/scenes/ExampleScene.java
+++ src/main/java/com/alice/mel/scenes/ExampleScene.java
@@ -10,6 +10,7 @@ import com.alice.mel.systems.RenderingSystem;
 import com.alice.mel.utils.maths.MathUtils;
 import com.github.sarxos.webcam.Webcam;
 import org.joml.Vector2f;
+import org.joml.Vector2i;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.Objects;
@@ -19,7 +20,8 @@ public class ExampleScene extends SceneAdaptor {
     public ExampleScene(Game game) {
         super(game);
     }
-
+    Window w;
+    Window w2;
     @Override
     public void Init(Window loaderWindow) {
 
@@ -33,8 +35,8 @@ public class ExampleScene extends SceneAdaptor {
         game.assetManager.addTexture("Texture2", textureC);
         game.assetManager.addMesh("Mesh1", mesh);
 
-        Window w = createWindow(CameraType.Orthographic, "Test", 640, 480, false);
-        Window w2 = createWindow(CameraType.Orthographic, "Test1", 640, 480, true);
+        w = createWindow(CameraType.Orthographic, "Test", 640, 480, false);
+        w2 = createWindow(CameraType.Orthographic, "Test1", 640, 480, true);
         Window w3 = createWindow(CameraType.Orthographic, "Test2", 640, 480, true);
 
 
@@ -59,7 +61,7 @@ public class ExampleScene extends SceneAdaptor {
 
     }
 
-    private final Vector2f move = new Vector2f(0,0);
+    private final Vector2i move = new Vector2i(0,0);
 
     @Override
     public void Update(float deltaTime) {
@@ -69,17 +71,24 @@ public class ExampleScene extends SceneAdaptor {
 
         move.set(0,0);
         if(getKey(GLFW.GLFW_KEY_W))
-            move.add(0,1);
-        if(getKey(GLFW.GLFW_KEY_S))
             move.add(0,-1);
+        if(getKey(GLFW.GLFW_KEY_S))
+            move.add(0,1);
         if(getKey(GLFW.GLFW_KEY_A))
             move.add(-1,0);
         if(getKey(GLFW.GLFW_KEY_D))
             move.add(1,0);
 
-        if(move.length() > 0) move.normalize().mul(deltaTime).mul(100f);
+        Vector2f cursorPos = w2.getCursorPosition();
+        Vector2i winPos = w2.getPosition();
+        Vector2i winSize = w2.getSize();
+        w2.setPosition((int)cursorPos.x  + winPos.x - winSize.x /2, (int)cursorPos.y + winPos.y - winSize.y /2);
+        System.out.println((int)cursorPos.x + " , " + (int)cursorPos.y);
 
-        scene.loaderWindow.camera.position.add(move.x, move.y, 0);
+
+        w.translate(move.x, move.y);
+
+
     }
 
     @Override
